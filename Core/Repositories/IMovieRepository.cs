9687db5bd069879b671030b409fb601573158dfb
Agustín Stringa <stringaagustin1@gmail.com>
2025-01-22 15:58:41 -0300

    custom repository created
    IRepository interface created
    IMovieRepository (custom repository) created

---
+++ Core/Repositories/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Core.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+	public interface IMovieRepository : IRepository<Movie>
+	{
+		Task<IEnumerable<Movie>> GetMoviesByDirectorName(string directorName);
+	}
+}
