e67a5e08740cc889596b3767fbcee2966330d718
Agustín Stringa <stringaagustin1@gmail.com>
2025-01-28 09:21:29 -0300

    Set up right ASP web project
    obsolete project removed

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,13 @@
-namespace API
+using Application.Mappers;
+using Core.Repositories.Base;
+using Core.Repositories;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Base;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace WebAPI
 {
 	public class Program
 	{
@@ -6,28 +15,32 @@ namespace API
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddRazorPages();
+			builder.Services.AddAuthorization();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+			builder.Services.AddControllers();
+			builder.Services.AddDbContext<MovieContext>(ctx => ctx.UseSqlServer(
+				builder.Configuration.GetConnectionString("MovieConnection")
+				), ServiceLifetime.Singleton);
+			builder.Services.AddAutoMapper(typeof(MovieMapperProfile));
+			//problems with mediatR and executing project or project that install mediatR
+			//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly));
+			builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+			builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+			builder.Services.AddTransient<IMovieRepository, MovieRepository>();
 
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
+			if (app.Environment.IsDevelopment())
 			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
+				app.UseSwagger();
+				app.UseSwaggerUI();
 			}
 
 			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-
-			app.UseRouting();
-
 			app.UseAuthorization();
-
-			app.MapRazorPages();
-
+			app.MapControllers();
 			app.Run();
 		}
 	}
