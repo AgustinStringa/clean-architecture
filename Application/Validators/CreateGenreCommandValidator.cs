e3ee0c119554c4f339d218b203b3804a32d82de0
Agustín Stringa <stringaagustin1@gmail.com>
2025-01-29 16:19:30 -0300

    Genre Validators

---
+++ Application/Validators/CreateGenreCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators
+{
+	public class CreateGenreCommandValidator : AbstractValidator<CreateGenreCommand>
+	{
+		public CreateGenreCommandValidator()
+		{
+			RuleFor(x => x.Name)
+			.NotEmpty().WithMessage("The genre's name is required")
+			.MaximumLength(65).WithMessage("The genre's name must not exceed 255 characters.")
+			;
+		}
+	}
+}
---
+++ Application/Validators/UpdateGenreCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators
+{
+	public class UpdateGenreCommandValidator : AbstractValidator<UpdateGenreCommand>
+	{
+		public UpdateGenreCommandValidator()
+		{
+			RuleFor(x => x.Id)
+			.NotEmpty().WithMessage("Genre's Id is required")
+			.GreaterThanOrEqualTo(0)
+			;
+
+			RuleFor(x => x.Name)
+			.NotEmpty().WithMessage("Genre's name is required")
+			.MaximumLength(65).WithMessage("Genre's name must not exceed 65 characters.");
+		}
+	}
+}
