623cc415416da48a65177fa3a444875fb7ba6a60
Agustín Stringa <stringaagustin1@gmail.com>
2025-01-29 16:18:53 -0300

    GenreCommands

---
+++ Application/Commands/CreateGenreCommand.cs
@@ -0,0 +1,19 @@
+﻿using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+	public class CreateGenreCommand : IRequest<GenreResponse>
+	{
+		public string Name { get; set; }
+		public CreateGenreCommand(string name)
+		{
+			Name = name;
+		}
+	}
+}
---
+++ Application/Commands/DeleteGenreCommand.cs
@@ -0,0 +1,20 @@
+﻿using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+	public class DeleteGenreCommand : IRequest<GenreResponse>
+	{
+		public int Id { get; set; }
+
+		public DeleteGenreCommand(int id)
+		{
+			Id = id;
+		}
+	}
+}
